cf53ae8e82dfe8276d79cfdb049db4fd275b3925
Tharshanth <tharsanthsanth4@gmail.com>
2020-11-07 13:39:05 +0530

    laravel-passport-api

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::group([
+    'prefix' => 'v1'
+], function ($router) {
+    Route::post('auth/login', [AuthController::class, 'login']);
+});
+
+
+Route::prefix('v1')->middleware(['auth:api','scope:admin,moderator,user', 'role'])->group(function () {
+    
+});
